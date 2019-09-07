19287d378b5c4910b172f2abee2d49caa59d430b
zt zou <zouzhitao_cq@163.com>
2019-09-07 17:01:32 +0800

    lazy page allocation

--- mmu.h
+++ mmu.h
@@ -4,6 +4,11 @@
 // Eflags register
 #define FL_IF           0x00000200      // Interrupt Enable
 
+// Page fault error codes
+#define FEC_PR		0x1	// Page fault caused by protection violation
+#define FEC_WR		0x2	// Page fault caused by a write
+#define FEC_U		  0x4	// Page fault occured while in user mode
+
 // Control Register flags
 #define CR0_PE          0x00000001      // Protection Enable
 #define CR0_WP          0x00010000      // Write Protect
